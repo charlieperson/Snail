a2ddbf2899b59e0eceec8ab0914d4d9b30cd3cf3
charlieperson <charlie.person@hyperisland.se>
2016-06-15 15:20:19 -0400

    Inserts bottom line into solution array backwards

--- js/Snail.js
+++ js/Snail.js
@@ -2,25 +2,28 @@ function Snail() {
 }
 
 Snail.prototype.spiral = function(input){
-
   var solution = input[0];
   var snailRemaining = input.splice(1, input.length);
 
-  console.log("solution = ");
-  console.log(solution);
-
-  console.log("snail remaining = ");
-  console.log(snailRemaining);
 
   console.log('enter for loop');
   for(i=0; i<snailRemaining.length; i++) {
-    console.log('current item = ' + snailRemaining[i]);
-    solution.push(snailRemaining[i].slice(-1)[0]);
-    console.log("current solution = " + solution);
+    var rowLength = snailRemaining[i].length;
+    solution.push(snailRemaining[i][rowLength-1]);
+    snailRemaining[i].splice(rowLength-1, 1);
   }
+
+  var bottom = snailRemaining[snailRemaining.length-1].reverse();
+
+  for(i=0; i<bottom.length; i++) {
+    solution.push(bottom[i]);
+  }
+  console.log(solution);
 };
 
-array = [[1,2,3,6],
-         [4,5,6,5],
-         [7,8,9,4],
-         [7,8,9,4]];
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]];
+
+array = [[4,5],
+         [7,8]];
