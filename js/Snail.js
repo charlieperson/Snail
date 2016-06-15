c65840b7823e5e40057067cba6b086357aa74038
charlieperson <charlie.person@hyperisland.se>
2016-06-15 15:31:57 -0400

    Removes unnecessary refactored variable in for loop

--- js/Snail.js
+++ js/Snail.js
@@ -5,25 +5,23 @@ Snail.prototype.spiral = function(input){
   var solution = input[0];
   var snailRemaining = input.splice(1, input.length);
 
-
-  console.log('enter for loop');
   for(i=0; i<snailRemaining.length; i++) {
-    var rowLength = snailRemaining[i].length;
-    solution.push(snailRemaining[i][rowLength-1]);
-    snailRemaining[i].splice(rowLength-1, 1);
+    solution.push(snailRemaining[i][snailRemaining[i].length-1]);
+    snailRemaining[i].splice(snailRemaining[i].length-1, 1);
   }
 
   var bottom = snailRemaining[snailRemaining.length-1].reverse();
+  snailRemaining.splice(snailRemaining.length-1, 1);
 
   for(i=0; i<bottom.length; i++) {
     solution.push(bottom[i]);
   }
   console.log(solution);
+  console.log(snailRemaining);
 };
 
 array = [[1,2,3],
          [4,5,6],
          [7,8,9]];
 
-array = [[4,5],
-         [7,8]];
+array = [[4,5]];
