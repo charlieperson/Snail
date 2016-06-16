4e5f9ce99fce027a24c436fc9a7a39ece1f3f2e8
charlieperson <charlie.person@hyperisland.se>
2016-06-16 10:42:13 -0500

    Adds failing test for a 4x4 array

--- spec/SnailSpec.js
+++ spec/SnailSpec.js
@@ -1,7 +1,12 @@
 describe("Snail", function() {
 
-  it("basic 3x3 array can successfully be snailed", function(){
+  it("can successfully snail a 3x3 array", function(){
     var snail = new Snail();
     expect(snail.spiral([[1,2,3],[4,5,6],[7,8,9]])).toEqual([1,2,3,6,9,8,7,4,5]);
+  });
+
+  it("can successfully snail a 4x4 array", function(){
+    var snail = new Snail();
+    expect(snail.spiral([[1,2,3,1],[4,5,6,4],[7,8,9,7],[7,8,9,7]])).toEqual([1,2,3,1,4,7,7,9,8,7,7,4,5,6,9,8]);
   });
 });
